575d2db67a0f0113c248287805d70539c212e337
Natalie Perpepaj <perpepajn25@gmail.com>
2017-08-22 17:10:35 -0400

    Updated Adapter Class

--- app/models/good_reads_adapter.rb
+++ app/models/good_reads_adapter.rb
@@ -7,7 +7,7 @@ module GoodReads
       @user_book_input = user_book_input
     end
 
-    def create_book
+    def find_or_create_book
       #makes request to API
       api = Goodreads::Client.new(:api_key => 'ytzqy6IgxnxFr4ieq6TCw', :api_secret => 'WntJehcPvpnI6ynAqBmK8tQ391Nb7o00FsLQXEH5U')
       search = api.search_books(self.user_book_input)
@@ -18,7 +18,8 @@ module GoodReads
       title = book.best_book.title
       author = book.best_book.author.name
 
-      Book.new(title, author, good_reads_rating)
+      Book.find_or_create_by(title: title, author: author, good_reads_rating: good_reads_rating)
     end
+
   end
 end

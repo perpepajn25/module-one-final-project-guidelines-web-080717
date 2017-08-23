2e26a42a154c2718888c7617fb741dade1cf4584
github username <logemerson@gmail.com>
2017-08-23 07:31:29 -0400

    added to CLI class: self.get_user, self.greet_user, name of self.choose_book, added prompt to retrieve book from user in self.choose_book

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -1,7 +1,6 @@
 class CLI
 
   def self.welcome
-
     puts "Welcome to Bad_Reads!"
     puts "This is the place where you smear books."
   end
@@ -12,9 +11,18 @@ class CLI
     User.find_or_create_by(username: username)
   end
 
-  def self.find_or_create_book(user_book_input)
+  def self.greet_user
+    puts "Welcome, #{user}!"
+  end
+
+  def self.choose_book(user_book_input)
+    #probably remove input to this method, get from CLI chomps!
+    puts "What book would you like to warn people about?"
+    ##perhaps we could randomize the output here from an array of a few different phrasings of the question (e.g."What book would you like to smear today?", "You look ready to destroy a book. What title would you like to smear?", etc.)
+    user_book_input = gets.chomp
         #makes request to API
     api = Goodreads::Client.new(:api_key => 'ytzqy6IgxnxFr4ieq6TCw', :api_secret => 'WntJehcPvpnI6ynAqBmK8tQ391Nb7o00FsLQXEH5U')
+
     search = api.search_books(user_book_input)
 
         #retrieves data
